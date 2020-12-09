f273375db24e1b35f467169a895fa44080ba707b
Andrew Throener <athroener@paypal.com>
2020-12-09 13:57:30 -0600

    Quick Release

--- src/payments/v2/index.ts
+++ src/payments/v2/index.ts
@@ -1,6 +1,5 @@
 import { CONFIG } from "../../config";
 import { IPayPalAccessToken } from "../../oauth";
-import { DEFAULT_PAYMENT_CAPTURE_PAYLOAD } from "../../payments/constants";
 
 export async function capture(
   token: IPayPalAccessToken,
@@ -8,10 +7,7 @@ export async function capture(
   data?: any,
   headers?: any
 ) {
-  const payload =
-    data && Object.keys(data).length > 0
-      ? data
-      : DEFAULT_PAYMENT_CAPTURE_PAYLOAD;
+  const payload = data || {};
 
   const options = {
     method: "POST",
@@ -26,6 +22,50 @@ export async function capture(
     `${CONFIG.get(
       "PAYPAL_REST_HOSTNAME"
     )}/v2/payments/authorizations/${id}/capture`,
+    options
+  );
+}
+
+export async function refund(
+  token: IPayPalAccessToken,
+  id: string,
+  data: any,
+  headers?: any
+) {
+  const payload = data || {};
+
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+      "Content-Type": "application/json",
+      ...headers
+    },
+    body: JSON.stringify(payload)
+  };
+  return await fetch(
+    `${CONFIG.get("PAYPAL_REST_HOSTNAME")}/v2/payments/captures/${id}/refund`,
+    options
+  );
+}
+
+export async function cancel(
+  token: IPayPalAccessToken,
+  id: string,
+  headers?: any
+) {
+  const options = {
+    method: "POST",
+    headers: {
+      Authorization: `Bearer ${token.access_token}`,
+      "Content-Type": "application/json",
+      ...headers
+    }
+  };
+  return await fetch(
+    `${CONFIG.get(
+      "PAYPAL_REST_HOSTNAME"
+    )}/v2/payments/authorizations/${id}/void`,
     options
   );
 }
