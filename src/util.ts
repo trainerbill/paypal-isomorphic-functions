926c1e3ca5a8fa4d2a3f86fbcbe5ab49b4f342b6
Andrew Throener <athroener@gmail.com>
2021-04-19 11:56:23 -0500

    Quick Release

--- src/util.ts
+++ src/util.ts
@@ -2,10 +2,10 @@ export function randomString() {
   return (Math.random() * 1e18).toString(36);
 }
 
-function randomAmount() {
+export function randomAmount() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-function randomAmountasString() {
+export function randomAmountasString() {
   return `${Math.floor(Math.random() * 100) + 1}.00`;
 }
