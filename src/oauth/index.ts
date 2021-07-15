9b2367a659085d2358a884f829b14d60d21e7d8c
Andrew Throener <athroener@gmail.com>
2021-07-15 11:50:02 -0500

    Quick Release

--- src/oauth/index.ts
+++ src/oauth/index.ts
@@ -26,11 +26,34 @@ export async function createAccessToken() {
   return response;
 }
 
-export async function getUserInfo() {
+export async function createAccessTokenFromCode(code: string) {
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-Language": "en_US",
+      Authorization: `Basic ${CONFIG.get("PAYPAL_REST_BEARER")}`,
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: `grant_type=authorization_code&code=${code}`
+  };
+
+  const res = await fetch(
+    `${CONFIG.get("PAYPAL_REST_HOSTNAME")}/v1/oauth2/token`,
+    options
+  );
+
+  const response: IPayPalAccessToken = await res.json();
+  response.expires_at = Date.now() + response.expires_in;
+
+  return response;
+}
+
+export async function getUserInfo(token: IPayPalAccessToken) {
   const options = {
     method: "GET",
     headers: {
-      Authorization: `Basic ${CONFIG.get("PAYPAL_REST_BEARER")}`,
+      Authorization: `Bearer ${token.access_token}`,
       "Content-Type": "application/json"
     }
   };
